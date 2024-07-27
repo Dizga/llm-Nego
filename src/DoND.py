467936a7698fe77b2ecee9d40b74a930f8aea435
Dereck Piché <pichedereck@gmail.com>
2024-07-27 16:59:27 -0400

    .

--- src/DoND.py
+++ src/DoND.py
@@ -30,7 +30,7 @@ class DoND():
             self.has_proposed = True
             return -1 # game ended because of wrong proposal
         # no proposal, continue conversation
-        if check_if_message(self, output): # check message format
+        if re.match(r"\[ Message \] .*", output): # check message format
             return output
         return -2 # game ended because of bad formatting
 
@@ -49,7 +49,7 @@ class DoND():
 
     def get_description(self, player) -> str:
         "Add instructions to model input string"
-        if self.current_turn() == "p0":
+        if player == "p0":
             return  f"""
                     There is a total of {self.quantities['books']} books,
                     {self.quantities['hats']} hats and {self.quantities['balls']} balls.
@@ -64,14 +64,9 @@ class DoND():
                     {self.values_p1['hat']} for a hat and {self.values_p1['ball']} for a ball.
             """
 
-    def check_if_message(self, string: str) -> bool:
-        # TODO: fix
-        return re.search(r"\[ Message \] .*", string)
-
     def propose(self, string: str) -> bool:
         "Sets proposal."
-        # TODO: fix
-        if re.search(r"\[ Proposal \] \{'books': \d+, 'hats': \d+, 'balls': \d+\}", string) == False:
+        if re.match(r"\[ Proposal \] \{'books': \d+, 'hats': \d+, 'balls': \d+\}", string) == False:
             return False
         prop = json.loads(string)
         if prop['books'] > self.quantities['books']: return False
@@ -83,10 +78,19 @@ class DoND():
 
     def reset(self):
         self.turn = 0
-        self.has_proposed = False
+        self.quantities = {'books': 5, 'hats': 4, 'balls': 3}
+        self.values_p0 = {'books': 5, 'hats': 4, 'balls':3}
+        self.values_p1 = {'books': 5, 'hats': 4, 'balls':3}
+        self.has_proposed = False # whether at least one player has proposed
+        self.p0_prop = {}
+        self.p1_prop = {}
+        self.points_p0 = 0
+        self.points_p1 = 0
+        self.agreement_reached = False
 
     def render(self):
         "Render the interations without chain of thought."
+        pass
 
     def export(self):
         "Export the game in Json Format."
@@ -102,5 +106,5 @@ class DoND():
         }
 
     def current_turn(self):
-        if self.turn % 2==0: return "a"
-        return "b"
+        if self.turn % 2==1: return "p0"
+        return "p1"
