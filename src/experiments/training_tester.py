62ee7595eaaf7aade639e292a6ecd08bb538e4b1
dereckpiche <pichedereck@gmail.com>
2024-09-17 19:50:16 -0400

    started testing white dragon cat

--- src/experiments/training_tester.py
+++ src/experiments/training_tester.py
@@ -87,12 +87,12 @@ def training_tester(cfg):
 
             # TODO: repeat the queries, responses, and scores 100 times (since there are only 2)
             # Filled TODO:
-            repeats = 2  # Since there are 2 items, 2 * 50 = 100
+            repeats = 20  # Since there are 2 items, 2 * 50 = 100
             queries *= repeats
             responses *= repeats
             scores *= repeats
 
-            for i in range(2):
+            for i in range(1000):
                 model.train_ppo(
                     '/home/mila/d/dereck.piche/llm-Nego/src/experiments/data', 
                     queries, 
@@ -103,6 +103,7 @@ def training_tester(cfg):
                 # Filled TODO:
                 output = model.prompt(good_query)
                 logging.info(f"Model output after iteration {i}: {output}")
+                model.switch_to_hf()
 
 
     # Calculate and log total duration
